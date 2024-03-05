55d8a3ce4febf28b09bb8c4158534d657967f6a6
tjs66 <119444886+tjs66@users.noreply.github.com>
2024-03-05 18:24:47 -0500

    -urlchange

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static('.'));
 
 // Route for the root path to directly serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/node/303`);
 });
 
 // Start the server
